8791319a2168a8e29113db9103b39efe3d4938c6
yagizyilmazen-coder <yagizyilmazen@gmail.com>
2026-07-17 23:49:04 +0300

    düzeltme YETER ARTIIIIIK

--- src/main/java/com/yagiz/skinpowers/PvpBotSystem.java
+++ src/main/java/com/yagiz/skinpowers/PvpBotSystem.java
@@ -17,7 +17,7 @@ import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.entity.EquipmentSlot;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.ai.attributes.Attributes;
-import net.minecraft.world.entity.monster.Husk;
+import net.minecraft.world.entity.monster.zombie.Husk;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.Items;
 import net.minecraft.world.phys.AABB;
